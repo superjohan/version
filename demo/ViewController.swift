b5fa956a5992b9b6d44a63f4660bec8577c8d2bf
Johan Halin <johan@halin.me>
2018-03-12 20:27:17 +0200

    Some setup for playing music, setting up SceneKit, a start button, etc.

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -7,11 +7,152 @@
 //
 
 import UIKit
+import AVFoundation
+import SceneKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SCNSceneRendererDelegate {
+    let audioPlayer: AVAudioPlayer
+    let sceneView = SCNView()
+    let camera = SCNNode()
+    let startButton: UIButton
+    let qtFoolingBgView: UIView = UIView.init(frame: CGRect.zero)
+
     // MARK: - UIViewController
+    
+    init() {
+        if let trackUrl = Bundle.main.url(forResource: "audio", withExtension: "m4a") {
+            guard let audioPlayer = try? AVAudioPlayer(contentsOf: trackUrl) else {
+                abort()
+            }
+            
+            self.audioPlayer = audioPlayer
+        } else {
+            abort()
+        }
+        
+        let camera = SCNCamera()
+        camera.zFar = 600
+//        camera.vignettingIntensity = 1
+//        camera.vignettingPower = 1
+//        camera.colorFringeStrength = 3
+//        camera.bloomIntensity = 1
+//        camera.bloomBlurRadius = 40
+        self.camera.camera = camera // lol
+        
+        let startButtonText =
+            "\"some demo\"\n" +
+                "by dekadence\n" +
+                "\n" +
+                "programming and music by ricky martin\n" +
+                "\n" +
+                "presented at some party 2018\n" +
+                "\n" +
+        "tap anywhere to start"
+        self.startButton = UIButton.init(type: UIButtonType.custom)
+        self.startButton.setTitle(startButtonText, for: UIControlState.normal)
+        self.startButton.titleLabel?.numberOfLines = 0
+        self.startButton.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+        self.startButton.backgroundColor = UIColor.black
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        self.startButton.addTarget(self, action: #selector(startButtonTouched), for: UIControlEvents.touchUpInside)
+        
+        self.view.backgroundColor = .black
+        self.sceneView.backgroundColor = .black
+        self.sceneView.delegate = self
+        
+        self.qtFoolingBgView.backgroundColor = UIColor(white: 0.1, alpha: 1.0)
+        
+        // barely visible tiny view for fooling Quicktime player. completely black images are ignored by QT
+        self.view.addSubview(self.qtFoolingBgView)
+        
+        self.view.addSubview(self.sceneView)
+
+        self.view.addSubview(self.startButton)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prefersHomeIndicatorAutoHidden() -> Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.audioPlayer.prepareToPlay()
+        
+        self.sceneView.scene = createScene()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.qtFoolingBgView.frame = CGRect(
+            x: (self.view.bounds.size.width / 2) - 1,
+            y: (self.view.bounds.size.height / 2) - 1,
+            width: 2,
+            height: 2
+        )
+
+        self.sceneView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+        self.sceneView.isPlaying = true
+        self.sceneView.isHidden = true
+
+        self.startButton.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
+    
+    // MARK: - SCNSceneRendererDelegate
+
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        // this function is run in a background thread.
+//        DispatchQueue.main.async {
+//        }
+    }
+    
+    // MARK: - Private
+    
+    @objc
+    fileprivate func startButtonTouched(button: UIButton) {
+        self.startButton.isUserInteractionEnabled = false
+        
+        // long fadeout to ensure that the home indicator is gone
+        UIView.animate(withDuration: 4, animations: {
+            self.startButton.alpha = 0
+        }, completion: { _ in
+            self.start()
+        })
+    }
+    
+    fileprivate func start() {
+        self.sceneView.isHidden = false
+        
+        self.audioPlayer.play()
+    }
+    
+    fileprivate func createScene() -> SCNScene {
+        let scene = SCNScene()
+        scene.background.contents = UIColor.black
+        
+        self.camera.position = SCNVector3Make(0, 0, 58)
+        
+        scene.rootNode.addChildNode(self.camera)
+        
+        configureLight(scene)
+        
+        return scene
+    }
+    
+    fileprivate func configureLight(_ scene: SCNScene) {
+        let omniLightNode = SCNNode()
+        omniLightNode.light = SCNLight()
+        omniLightNode.light?.type = SCNLight.LightType.omni
+        omniLightNode.light?.color = UIColor(white: 1.0, alpha: 1.0)
+        omniLightNode.position = SCNVector3Make(0, 0, 60)
+        scene.rootNode.addChildNode(omniLightNode)
     }
 }
