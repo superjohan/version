6babebe5161232fdfb608faeb191372d135ce1b1
Johan Halin <johan@halin.me>
2018-03-18 20:36:18 +0200

    Three more brands.
    asfsajfaslfallksadlkfds

---
+++ demo/brands/quantifex/BrandViewQuantifex.swift
@@ -0,0 +1,38 @@
+//
+//  BrandViewQuantifex.swift
+//  demo
+//
+//  Created by Johan Halin on 18/03/2018.
+//  Copyright © 2018 Dekadence. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BrandViewQuantifex: UIView, BrandView {
+    // MARK: - UIView
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadImages(view: self, name: "quantifex", count: 2)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - BrandView
+    
+    func showBrand() {
+        for view in self.subviews {
+            view.isHidden = false
+        }
+    }
+    
+    func animateBrand() {
+        for view in self.subviews {
+            view.isHidden = true
+        }
+    }
+}
---
+++ demo/brands/santafelos/BrandViewSantafelos.swift
@@ -0,0 +1,38 @@
+//
+//  BrandViewSantafelos.swift
+//  demo
+//
+//  Created by Johan Halin on 18/03/2018.
+//  Copyright © 2018 Dekadence. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BrandViewSantafelos: UIView, BrandView {
+    // MARK: - UIView
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadImages(view: self, name: "santafelos", count: 2)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - BrandView
+    
+    func showBrand() {
+        for view in self.subviews {
+            view.isHidden = false
+        }
+    }
+    
+    func animateBrand() {
+        for view in self.subviews {
+            view.isHidden = true
+        }
+    }
+}
