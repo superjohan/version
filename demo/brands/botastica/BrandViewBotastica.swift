cb08fd82aa038c73d74c4b4c32cbb912aae0352d
Johan Halin <johan@halin.me>
2018-03-19 19:25:21 +0200

    Botastica brand animation.

--- demo/brands/botastica/BrandViewBotastica.swift
+++ demo/brands/botastica/BrandViewBotastica.swift
@@ -31,8 +31,21 @@ class BrandViewBotastica: UIView, BrandView {
     }
     
     func animateBrand() {
-        for view in self.subviews {
-            view.isHidden = true
-        }
+        let length = Constants.barLength / 5.0
+        let delay = length / 2.0
+        
+        let ball = self.subviews[0]
+        ball.alpha = 0
+        ball.frame.origin.y = ball.frame.origin.y - 80
+        UIView.animate(withDuration: length, delay: delay, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+            ball.alpha = 1
+            ball.frame = self.frame
+        }, completion: nil)
+        
+        let logo = self.subviews[1]
+        logo.alpha = 0
+        UIView.animate(withDuration: length, delay: 0, options: [ .curveEaseInOut ], animations: {
+            logo.alpha = 1
+        }, completion: nil)
     }
 }
