c0a3403d1a634891f4d45a870fe17a55b7e60b67
Johan Halin <johan@halin.me>
2018-03-19 20:12:13 +0200

    Freemo brand animation.

--- demo/brands/freemo/BrandViewFreemo.swift
+++ demo/brands/freemo/BrandViewFreemo.swift
@@ -31,8 +31,18 @@ class BrandViewFreemo: UIView, BrandView {
     }
     
     func animateBrand() {
-        for view in self.subviews {
-            view.isHidden = true
+        let length = Constants.barLength / 5.0
+        let delay = length / 8.0
+        
+        for i in 0..<self.subviews.count {
+            let view = self.subviews[i]
+            view.alpha = 0
+            view.frame.origin.x = self.bounds.size.width
+            
+            UIView.animate(withDuration: length, delay: delay * Double(i), options: [ .curveEaseInOut ], animations: {
+                view.alpha = 1
+                view.frame.origin.x = 0
+            }, completion: nil)
         }
     }
 }
