5745c2bfb3b05e5139a6e4b592222babe41b3664
Johan Halin <johan@halin.me>
2018-03-21 20:51:15 +0200

    Zophario brand animation.

--- demo/brands/zophario/BrandViewZophario.swift
+++ demo/brands/zophario/BrandViewZophario.swift
@@ -31,8 +31,18 @@ class BrandViewZophario: UIView, BrandView {
     }
     
     func animateBrand() {
-        for view in self.subviews {
-            view.isHidden = true
+        let length = Constants.barLength / 6.0
+        let delay = length / 8.0
+        
+        for i in 0..<self.subviews.count {
+            let view = self.subviews[i]
+            view.alpha = 0
+            view.frame.origin.x = i % 2 == 0 ? -self.bounds.size.width : self.bounds.size.width
+            
+            UIView.animate(withDuration: length, delay: delay * Double(i), options: [ .curveEaseInOut ], animations: {
+                view.alpha = 1
+                view.frame = self.bounds
+            }, completion: nil)
         }
     }
 }
