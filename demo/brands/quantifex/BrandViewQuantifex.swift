a2c9d72f9cf5683756c8822b887657c1c99af39f
Johan Halin <johan@halin.me>
2018-03-21 19:06:23 +0200

    Quantifex brand animation.

--- demo/brands/quantifex/BrandViewQuantifex.swift
+++ demo/brands/quantifex/BrandViewQuantifex.swift
@@ -31,8 +31,35 @@ class BrandViewQuantifex: UIView, BrandView {
     }
     
     func animateBrand() {
-        for view in self.subviews {
-            view.isHidden = true
+        let length = Constants.barLength / 5.0
+        let delay = length / 5.0
+        
+        let logo = self.subviews[0]
+        logo.alpha = 0
+        UIView.animate(withDuration: length, delay: 0, options: [ .curveEaseInOut ], animations: {
+            logo.alpha = 1
+        }, completion: nil)
+        
+        for i in 1..<self.subviews.count {
+            let view = self.subviews[i]
+            view.alpha = 0
+            view.transform = CGAffineTransform.init(scaleX: 0, y: 0)
+            
+            let delay2: TimeInterval
+            if i == 1 {
+                delay2 = delay * 2
+            } else if i == 2 {
+                delay2 = delay
+            } else if i == 3 {
+                delay2 = delay * 3
+            } else {
+                delay2 = 0
+            }
+            
+            UIView.animate(withDuration: length, delay: delay2, options: [ .curveEaseInOut ], animations: {
+                view.alpha = 1
+                view.transform = CGAffineTransform.identity
+            }, completion: nil)
         }
     }
 }
