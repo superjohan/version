d7733b0927c2cc8691831d754e03c848c38d53db
Johan Halin <johan@halin.me>
2018-03-22 20:28:13 +0200

    Placeholder scene with a short camera flyby.
    The camera animation is paused while it's not visible.

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -224,6 +224,8 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         self.sceneView.isHidden = true
         self.brandViewContainer.isHidden = true
         self.isInMiddleState = false
+
+        self.camera.isPaused = true
     }
     
     @objc
@@ -232,6 +234,8 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         self.sceneView.isHidden = false
         self.brandViewContainer.isHidden = true
         self.isInMiddleState = false
+
+        self.camera.isPaused = false
     }
     
     @objc
@@ -240,7 +244,9 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         self.sceneView.isHidden = true
         self.brandViewContainer.isHidden = false
         self.isInMiddleState = false
-        
+
+        self.camera.isPaused = true
+
         self.brandViewContainer.showBrand(self.brandOrder[self.brandPosition], animated: true)
         
         self.brandPosition += 1
@@ -266,21 +272,68 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         let scene = SCNScene()
         scene.background.contents = UIColor.black
         
-        self.camera.position = SCNVector3Make(0, 0, 58)
+        self.camera.position = SCNVector3Make(-50, 60, 100)
+        self.camera.rotation = SCNVector4Make(1, 1, 0, -0.4)
+        let duration: TimeInterval = Constants.beatLength * 32
         
+        let cameraMoveAction = SCNAction.move(to: SCNVector3Make(-40, 10, 0), duration: duration)
+        cameraMoveAction.timingMode = SCNActionTimingMode.easeInEaseOut
+        self.camera.runAction(cameraMoveAction)
+        
+        let cameraRotateAction = SCNAction.rotateBy(x: 0.4, y: -1.3, z: 0, duration: duration)
+        cameraRotateAction.timingMode = cameraMoveAction.timingMode
+        self.camera.runAction(cameraRotateAction)
+
         scene.rootNode.addChildNode(self.camera)
         
         configureLight(scene)
         
+        let box = SCNBox(width: 20, height: 20, length: 20, chamferRadius: 0)
+        box.firstMaterial?.diffuse.contents = UIColor.white
+
+        let boxNode = SCNNode(geometry: box)
+        boxNode.position = SCNVector3Make(0, 20, 0)
+
+        boxNode.runAction(
+            SCNAction.repeatForever(
+                SCNAction.rotateBy(
+                    x: CGFloat(-10 + Int(arc4random_uniform(20))),
+                    y: CGFloat(-10 + Int(arc4random_uniform(20))),
+                    z: CGFloat(-10 + Int(arc4random_uniform(20))),
+                    duration: TimeInterval(8 + arc4random_uniform(5))
+                )
+            )
+        )
+
+        scene.rootNode.addChildNode(boxNode)
+
+        let box2 = SCNBox(width: 20, height: 100, length: 20, chamferRadius: 0)
+        box2.firstMaterial?.diffuse.contents = UIColor.gray
+        
+        let boxNode2 = SCNNode(geometry: box2)
+        boxNode2.position = SCNVector3Make(0, -50, 0)
+        
+        scene.rootNode.addChildNode(boxNode2)
+
         return scene
     }
     
     fileprivate func configureLight(_ scene: SCNScene) {
+        let directionalLightNode = SCNNode()
+        directionalLightNode.light = SCNLight()
+        directionalLightNode.light?.type = SCNLight.LightType.directional
+        directionalLightNode.light?.castsShadow = true
+        directionalLightNode.light?.shadowRadius = 30
+        directionalLightNode.light?.shadowColor = UIColor(white: 0, alpha: 0.75)
+        directionalLightNode.light?.color = UIColor(white: 1.0, alpha: 1.0)
+        directionalLightNode.position = SCNVector3Make(0, 20, 40)
+        directionalLightNode.rotation = SCNVector4Make(1, 0, 0, -0.75)
+        scene.rootNode.addChildNode(directionalLightNode)
+
         let omniLightNode = SCNNode()
         omniLightNode.light = SCNLight()
-        omniLightNode.light?.type = SCNLight.LightType.omni
-        omniLightNode.light?.color = UIColor(white: 1.0, alpha: 1.0)
-        omniLightNode.position = SCNVector3Make(0, 0, 60)
+        omniLightNode.light?.type = SCNLight.LightType.ambient
+        omniLightNode.light?.color = UIColor(white: 0.1, alpha: 1.0)
         scene.rootNode.addChildNode(omniLightNode)
     }
     
