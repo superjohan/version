b79eae188062f18c70d85e0a438c87290561e194
Johan Halin <johan@halin.me>
2018-03-25 20:00:46 +0300

    Simple util function for loading a Collada model, with optional texture.
    Very strict: it crashes if anything fails. But in demo use it makes sense imo. Should move to the template project.

---
+++ demo/SceneKitUtils.swift
@@ -0,0 +1,28 @@
+//
+//  SceneKitUtils.swift
+//  demo
+//
+//  Created by Johan Halin on 25/03/2018.
+//  Copyright © 2018 Dekadence. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+func loadModel(name: String, textureName: String?) -> SCNNode {
+    guard let filePath = Bundle.main.path(forResource: "paatti", ofType: "dae", inDirectory: "") else { abort() }
+    let referenceURL = URL(fileURLWithPath: filePath)
+    guard let referenceNode = SCNReferenceNode(url: referenceURL) else { abort() }
+    referenceNode.load()
+    
+    if let textureName = textureName {
+        let boatImage: UIImage? = UIImage(named: textureName)
+        let childNodes = referenceNode.childNodes
+        for childNode in childNodes {
+            childNode.geometry?.firstMaterial = SCNMaterial()
+            childNode.geometry?.firstMaterial?.diffuse.contents = boatImage
+        }
+    }
+    
+    return referenceNode
+}
